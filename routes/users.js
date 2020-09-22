6267ea0cbfc56303189df73b38560a083979807a
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2020-09-22 15:32:19 +0500

    Hashing Passwords

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const { User, validateUser } = require('../model/userModel');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -15,15 +16,14 @@ routes.post('/', async (req, res) => {
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('user already register');
 
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    });
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt)
 
     await user.save();
 
-    res.send(user);
+    res.send(_.pick(user, ['_id', 'name', 'email']));
 
 });
 
