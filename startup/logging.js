d5a5779b721ceb07a6d4b93866307c42b33da6d9
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2020-09-29 21:14:23 +0500

    Extracting the Validation Logic

--- startup/logging.js
+++ startup/logging.js
@@ -2,24 +2,17 @@ const winston = require('winston');
 require('winston-mongodb');
 require('express-async-errors');
 
-
-module.exports = function (){
-    
-process.on('uncaughtException',(ex)=>{
-    console.log('Unexpexted exception');
-    winston.error(ex.message,ex);
-    process.exit(1);
-});
-
-process.on('unhandledRejection',(ex)=>{
-    console.log('we got unhandle rejection');
-    winston.error(ex.message,ex);
-    process.exit(1);
-});
-
-winston.add(winston.transports.File, {filename:'logfile.log'});
-winston.add(winston.transports.MongoDB,{
-db:'mongodb://localhost/vidly',
-leve:'error'
-});
+module.exports = function() {
+  winston.handleExceptions(
+    new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
+  
+  process.on('unhandledRejection', (ex) => {
+    throw ex;
+  });
+  
+  winston.add(winston.transports.File, { filename: 'logfile.log' });
+  winston.add(winston.transports.MongoDB, { 
+    db: 'mongodb://localhost/vidly',
+    level: 'info'
+  });  
 }
