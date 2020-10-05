661ec360a6bce632cccbc2b1658fef80649b2c99
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2020-10-05 14:40:45 +0500

    Populating the Test DB

--- test/intrigation/genres.test.js
+++ test/intrigation/genres.test.js
@@ -1,13 +1,24 @@
 const request = require('supertest');
+const {Genre} = require('../../model/genreModel');
 let server;
 
 describe('/api/genres', () => {
     beforeEach(() => { server = require('../../index'); });
-    afterEach(() => { server.close()});
+    afterEach(async () => { 
+        server.close(),
+        await Genre.remove({});
+    });
     describe('GET /', () => {
         it('should return genres list', async () => {
+            await Genre.collection.insertMany([
+                {name:'genre1'},
+                {name:'genre2'},
+            ])
             const res = await request(server).get('/vidly.com/api/genres');
             expect(res.status).toBe(200);
+            expect(res.body.length).toBe(2);
+            expect(res.body.some(g=>g.name==='genre1')).toBeTruthy();
+            expect(res.body.some(g=>g.name==='genre2')).toBeTruthy();
         });
     });
 });
