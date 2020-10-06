380a9e615bb0b789a7ec4146e23ce82a309f55d8
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2020-10-06 13:11:01 +0500

    Testing Routes with Parameters

--- test/intrigation/genres.test.js
+++ test/intrigation/genres.test.js
@@ -1,24 +1,36 @@
+const { Genre } = require('../../model/genreModel');
 const request = require('supertest');
-const {Genre} = require('../../model/genreModel');
 let server;
 
 describe('/api/genres', () => {
     beforeEach(() => { server = require('../../index'); });
-    afterEach(async () => { 
+    afterEach(async () => {
         server.close(),
-        await Genre.remove({});
+            await Genre.remove({});
     });
     describe('GET /', () => {
         it('should return genres list', async () => {
             await Genre.collection.insertMany([
-                {name:'genre1'},
-                {name:'genre2'},
+                { name: 'genre1' },
+                { name: 'genre2' },
             ])
             const res = await request(server).get('/vidly.com/api/genres');
             expect(res.status).toBe(200);
             expect(res.body.length).toBe(2);
-            expect(res.body.some(g=>g.name==='genre1')).toBeTruthy();
-            expect(res.body.some(g=>g.name==='genre2')).toBeTruthy();
+            expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
+            expect(res.body.some(g => g.name === 'genre2')).toBeTruthy();
+        });
+    });
+
+    describe('GET /:id', () => {
+        it('should return a valid genre id if exsists', async () => {
+            const genre = new Genre({ name: 'genre1' });
+            await genre.save();
+
+           const res = await request(server).get('/vidly.com/api/genres/' + genre._id);
+           expect(res.status).toBe(200);
+           expect(res.body).toHaveProperty('name',genre.name);
+           
         });
     });
 });
